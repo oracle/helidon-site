7b4d27f22a5aa36f1589f298f87a5433f08b2bcc
Helidon Robot <robot@helidon.io>
2018-09-06 16:11:54 -0700

    first commit

---
+++ etc/scripts/build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -o pipefail || true  # trace ERR through pipes
+set -o errtrace || true # trace ERR through commands and functions
+set -o errexit || true  # exit the script if any statement returns a non-true return value
+
+on_error(){
+    CODE="${?}" && \
+    set +x && \
+    printf "[ERROR] Error(code=%s) occurred at %s:%s command: %s\n" \
+        "${CODE}" "${BASH_SOURCE}" "${LINENO}" "${BASH_COMMAND}"
+}
+trap on_error ERR
+
+# Path to this script
+if [ -h "${0}" ] ; then
+  readonly SCRIPT_PATH="$(readlink "${0}")"
+else
+  readonly SCRIPT_PATH="${0}"
+fi
+
+# Path to the root of the workspace
+readonly WS_DIR=$(cd $(dirname -- "${SCRIPT_PATH}") ; cd ../.. ; pwd -P)
+
+source ${WS_DIR}/etc/scripts/wercker-env.sh
+
+if [ "${WERCKER}" = "true" ] ; then
+  # Add private_key from IDENTITY_FILE
+  mkdir ~/.ssh/ 2>/dev/null || true
+  echo -e "${IDENTITY_FILE}" > ~/.ssh/id_rsa
+  chmod og-rwx ~/.ssh/id_rsa
+  echo -e "Host *" >> ~/.ssh/config
+  echo -e "\tStrictHostKeyChecking no" >> ~/.ssh/config
+  echo -e "\tUserKnownHostsFile /dev/null" >> ~/.ssh/config
+fi
+
+mvn -f ${WS_DIR}/pom.xml clean deploy -Ppublish,ossrh-releases
---
+++ src/main.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2018 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import VueScrollTo from 'vue-scrollto'
+
+Vue.use(VueScrollTo, { offset: -58 })
+
+Vue.use(Vuetify, { theme: {
+  primary: '#ee44aa',
+  secondary: '#424242',
+  accent: '#82B1FF',
+  error: '#FF5252',
+  info: '#2196F3',
+  success: '#4CAF50',
+  warning: '#FFC107'
+}})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
