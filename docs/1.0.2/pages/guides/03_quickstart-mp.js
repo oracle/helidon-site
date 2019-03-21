9e6620dbf9483cda38cdac54c87cc601c410426a
Helidon Robot <info@helidon.io>
2019-03-21 23:23:21 +0000

    Update site

---
+++ docs/1.0.2/pages/guides/03_quickstart-mp.js
@@ -0,0 +1,174 @@
+<doc-view>
+
+<v-layout row wrap>
+<v-flex xs12 sm10 lg10>
+<v-card class="section-def" v-bind:color="$store.state.currentColor">
+<v-card-text class="pa-3">
+<v-card class="section-def__card">
+<v-card-text>
+<dl>
+<dt slot=title>Quickstart MP</dt>
+<dd slot="desc"><p>This guide describes a basic example of an Helidon MP application using Docker
+ and Kubernetes.</p>
+</dd>
+</dl>
+</v-card-text>
+</v-card>
+</v-card-text>
+</v-card>
+</v-flex>
+</v-layout>
+
+<h2 id="_what_you_need">What you need</h2>
+<div class="section">
+
+<div class="table__overflow elevation-1 flex sm7
+">
+<table class="datatable table">
+<colgroup>
+<col style="width: 100%;">
+</colgroup>
+<thead>
+</thead>
+<tbody>
+<tr>
+<td>About 5 minutes</td>
+</tr>
+<tr>
+<td><router-link to="/about/03_prerequisites">Helidon Prerequisites</router-link></td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+
+<h2 id="_generate_the_project">Generate The Project</h2>
+<div class="section">
+<p>Generate the project sources using one (or both) of the Helidon Maven
+ archetypes. The result is a simple project that shows the basics of configuring
+ the WebServer and implementing basic routing rules.</p>
+
+<markup
+lang="bash"
+title="Run the Maven archetype"
+>mvn archetype:generate -DinteractiveMode=false \
+    -DarchetypeGroupId=io.helidon.archetypes \
+    -DarchetypeArtifactId=helidon-quickstart-mp \
+    -DarchetypeVersion=1.0.2 \
+    -DgroupId=io.helidon.examples \
+    -DartifactId=helidon-quickstart-mp \
+    -Dpackage=io.helidon.examples.quickstart.mp</markup>
+
+<p>The archetype generates a Maven project in your current directory
+(for example, <code>helidon-quickstart-mp</code>). Change into this directory.</p>
+
+<markup
+lang="bash"
+
+>cd helidon-quickstart-mp</markup>
+
+<div class="admonition tip">
+<p class="admonition-inline">If you want to use the generated project as a starter
+for your own application, then you can replace groupId, artifactId
+and package with values appropriate for your application.</p>
+</div>
+<markup
+lang="bash"
+title="Build the Application"
+>mvn package</markup>
+
+<p>The project builds an application jar for the example and saves all runtime
+dependencies in the <code>target/libs</code> directory. This means you can easily start the
+ application by running the application jar file:</p>
+
+<markup
+lang="bash"
+title="Run the application"
+>java -jar target/helidon-quickstart-mp.jar</markup>
+
+<p>The example is a very simple "Hello World" greeting service. It supports GET
+ requests for generating a greeting message, and a PUT request for changing the
+ greeting itself. The response is encoded using JSON.
+For example:</p>
+
+<markup
+lang="bash"
+title="Try the Application"
+>curl -X GET http://localhost:8080/greet
+{"message":"Hello World!"}
+
+curl -X GET http://localhost:8080/greet/Joe
+{"message":"Hello Joe!"}
+
+curl -X PUT -H "Content-Type: application/json" -d '{"greeting" : "Hola"}' http://localhost:8080/greet/greeting
+
+curl -X GET http://localhost:8080/greet/Jose
+{"message":"Hola Jose!"}</markup>
+
+</div>
+
+<h2 id="_build_a_docker_image">Build a Docker Image</h2>
+<div class="section">
+<p>The project also contains a Dockerfile so that you can easily
+build and run a Docker image. Because the example&#8217;s runtime
+dependencies are already in <code>target/libs</code>, the Dockerfile is
+pretty simple (see <code>target/Dockerfile</code>). To build the Docker
+image, you need to have Docker installed and running on your system.</p>
+
+<markup
+lang="bash"
+title="Docker build"
+>docker build -t helidon-quickstart-mp target</markup>
+
+<markup
+lang="bash"
+title="Run Docker Image"
+>docker run --rm -p 8080:8080 quickstart-mp:latest</markup>
+
+<p>Then you can try the application as you did before.</p>
+
+</div>
+
+<h2 id="_deploy_the_application_to_kubernetes">Deploy the application to Kubernetes</h2>
+<div class="section">
+<p>If you don&#8217;t have access to a Kubernetes cluster, you can
+<router-link to="#getting-started/05_kubernetes.adoc" @click.native="this.scrollFix('#getting-started/05_kubernetes.adoc')">install one on your desktop</router-link>.
+Then deploy the example:</p>
+
+<markup
+lang="bash"
+title="Verify connectivity to cluster"
+>kubectl cluster-info
+kubectl get nodes</markup>
+
+<markup
+lang="bash"
+title="Deploy the application to Kubernetes"
+>kubectl create -f target/app.yaml
+kubectl get pods                    # Wait for quickstart pod to be RUNNING</markup>
+
+<p>The step above created a service that is exposed into any node port. Lookup
+ the service to find the port.</p>
+
+<markup
+lang="bash"
+title="Lookup the service"
+>kubectl get service helidon-quickstart-mp</markup>
+
+<p>Note the PORTs. You can now exercise the application as you did before but use
+ the second port number (the NodePort) instead of 8080. For example:</p>
+
+<markup
+lang="bash"
+
+>curl -X GET http://localhost:31431/greet</markup>
+
+<p>After you&#8217;re done, cleanup.</p>
+
+<markup
+lang="bash"
+title="Remove the application from Kubernetes"
+>kubectl delete -f target/app.yaml</markup>
+
+</div>
+</doc-view>
---
+++ docs/1.0.2/pages/microprofile/01_introduction.js
@@ -0,0 +1,142 @@
+<doc-view>
+
+<v-layout row wrap>
+<v-flex xs12 sm10 lg10>
+<v-card class="section-def" v-bind:color="$store.state.currentColor">
+<v-card-text class="pa-3">
+<v-card class="section-def__card">
+<v-card-text>
+<dl>
+<dt slot=title>Microprofile Introduction</dt>
+<dd slot="desc"><p>MicroProfile is a platform definition that is familiar to Java EE developers. If you have experience with JAX-RS, JSON-P, and CDI, you
+may prefer to use this model.</p>
+
+<p>To extend the functionality of your MicroProfile application, you might also decide to use the Helidon core APIs, especially for
+configuration and security.</p>
+</dd>
+</dl>
+</v-card-text>
+</v-card>
+</v-card-text>
+</v-card>
+</v-flex>
+</v-layout>
+
+<h2 id="_getting_started_with_helidon_microprofile">Getting Started with Helidon MicroProfile</h2>
+<div class="section">
+<p>Complete these tasks to get started with your MicroProfile application.</p>
+
+
+<h3 id="_maven_coordinates">Maven Coordinates</h3>
+<div class="section">
+<p>Declare the following dependency in your project:</p>
+
+<markup
+lang="xml"
+title="Maven Dependency"
+>&lt;dependency&gt;
+  &lt;groupId&gt;io.helidon.microprofile.bundles&lt;/groupId&gt;
+  &lt;artifactId&gt;helidon-microprofile-1.2&lt;/artifactId&gt;
+&lt;/dependency&gt;</markup>
+
+</div>
+
+<h3 id="_project_files">Project files</h3>
+<div class="section">
+<p>Create a JAX-RS Resource class with at least one resource method.</p>
+
+<markup
+lang="java"
+title="Sample JAX-RS Resource Class"
+>@Path("/")
+@RequestScoped
+public class HelloWorldResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String message() {
+        return "Hello World";
+    }
+}</markup>
+
+<p>And create a JAX-RS application.</p>
+
+<markup
+lang="java"
+title="Sample JAX-RS Application"
+>@ApplicationScoped
+@ApplicationPath("/")
+public class HelloWorldApplication extends Application {
+    @Override
+    public Set&lt;Class&lt;?&gt;&gt; getClasses() {
+        return Set.of(
+                HelloWorldResource.class
+        );
+    }
+}</markup>
+
+<p>Add <code>beans.xml</code> in <code>src/main/resources/META-INF</code> so
+the CDI implementation can pick up your classes.</p>
+
+<markup
+lang="xml"
+title="beans.xml"
+>&lt;?xml version="1.0" encoding="UTF-8"?&gt;
+&lt;beans/&gt;</markup>
+
+<p>As a last step, add a main method to your application (or a dedicated Main class)
+ to start everything up.</p>
+
+<markup
+lang="java"
+title="Sample JAX-RS Application"
+>public static void main(String[] args) {
+    io.helidon.microprofile.server.Main.main(args);
+}</markup>
+
+<p>Run the main class. The server will start on port 7001 and serve your
+ resources.</p>
+
+</div>
+
+<h3 id="_adding_jandex">Adding Jandex</h3>
+<div class="section">
+<p>Jandex is an indexing tool for Weld (CDI implementation) that helps speed up
+the boot time of an application.</p>
+
+<p>To use Jandex, configure a Maven plugin that adds the index to your
+ JAR file and a dependency on Jandex.</p>
+
+<markup
+lang="xml"
+title="jandex dependency"
+>&lt;dependency&gt;
+    &lt;groupId&gt;org.jboss&lt;/groupId&gt;
+    &lt;artifactId&gt;jandex&lt;/artifactId&gt;
+    &lt;version&gt;2.0.4.Final&lt;/version&gt;
+&lt;/dependency&gt;</markup>
+
+<markup
+lang="xml"
+title="jandex plugin configuration"
+>&lt;build&gt;
+    &lt;plugins&gt;
+        &lt;plugin&gt;
+            &lt;groupId&gt;org.jboss.jandex&lt;/groupId&gt;
+            &lt;artifactId&gt;jandex-maven-plugin&lt;/artifactId&gt;
+            &lt;version&gt;1.0.5&lt;/version&gt;
+            &lt;executions&gt;
+                &lt;execution&gt;
+                    &lt;id&gt;make-index&lt;/id&gt;
+                    &lt;goals&gt;
+                        &lt;goal&gt;jandex&lt;/goal&gt;
+                    &lt;/goals&gt;
+                    &lt;phase&gt;process-classes&lt;/phase&gt;
+                &lt;/execution&gt;
+            &lt;/executions&gt;
+        &lt;/plugin&gt;
+    &lt;/plugins&gt;
+&lt;/build&gt;</markup>
+
+</div>
+</div>
+</doc-view>
